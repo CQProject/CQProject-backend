74283711a980e10f18c0b599a1a8390603041bf9
diogosantosmendes <diogo.santos.mendes@hotmail.com>
2017-05-17 11:00:49 +0100

    template with users

---
+++ CQPROJ/CQPROJ.Database/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CQPROJ.Database.Models
+{
+    public class User: IdentityUser
+    {
+
+    }
+}
