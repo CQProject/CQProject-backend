68a14d0119363667ebf37e2001712e2094835891
ricardma <aluno18774@ipt.pt>
2017-05-17 14:19:23 +0100

    Models Definition.

---
+++ CQPROJ/CQPROJ.Database/Models/Tbl_Schedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CQPROJ.Database.Models
+{
+    //DESCRIPTION:
+    //Refers to the Classes schedule
+    public class Tbl_Schedule
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public DateTime StartingTime { get; set; }
+
+        [Required]
+        public DateTime EndingTime { get; set; }
+    }
+}
