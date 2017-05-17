7e210be63007386cfca9a3f31ad0d8e496634d7c
diogosantosmendes <diogo.santos.mendes@hotmail.com>
2017-05-17 10:45:05 +0100

    Template

---
+++ CQPROJ/CQPROJ.Business/Exemple.cs
@@ -0,0 +1,17 @@
+﻿using CQPROJ.Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQPROJ.Business
+{
+    public class Exemple
+    {
+        public static User GetUserTeste() {
+            var user = CQPROJ.Data.AD.CQPROJ_NET.GetUser("teste");
+            return new User { Name = user.DisplayName };
+        }
+    }
+}
---
+++ CQPROJ/CQPROJ.Data.AD/CQPROJ_NET.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQPROJ.Data.AD
+{
+    public class CQPROJ_NET
+    {
+        public static Principal GetUser(string SamAccountName)
+        {
+            using (var context = new PrincipalContext(ContextType.Domain, "192.168.10.110", "CQPROJ\\teste", "zaq1\"WSX"))
+            {
+                using(var searcher=new PrincipalSearcher(new UserPrincipal(context)))
+                {
+                    searcher.QueryFilter.SamAccountName = SamAccountName;
+                    return searcher.FindOne();
+                }
+            }
+        }
+    }
+}
