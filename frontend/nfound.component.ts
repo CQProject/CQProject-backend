b4d082b614c29aa8a0d4a78e8a619c30078b6526
diogosantosmendes <diogo.santos.mendes@hotmail.com>
2017-05-23 18:22:16 +0100

    Frontend template

---
+++ frontend/nfound.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+	 template: `
+    <h2 style="color:#800">{{title}}</h2>
+    <hr>
+    <p  style="color:#800">{{error}}</p> 
+  `
+})
+
+export class NotFoundComponent{
+  title = '404';
+  error = 'Página Não encontrada!'
+}
