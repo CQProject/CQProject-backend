b82c3f17173b7eb54b861d852657c64c0d916c35
ricardma <aluno18774@ipt.pt>
2017-05-24 16:57:27 +0100

    Controllers changes

---
+++ API/CQPROJ.Business/Queries/Student.cs
@@ -0,0 +1,18 @@
+﻿using CQPROJ.Data.BD.Models;
+using System;
+using System.Linq;
+
+namespace CQPROJ.Business.Queries
+{
+    public class Student
+    {
+        private ModelsDbContext db = new ModelsDbContext();
+
+        public Object GetStudent(int id)
+        {
+            var stud = db.TblStudents.Select(x=>x).Where( x=> x.ID.Equals(id));
+            var student = stud.Select(x => new { x.Photo,x.DataOfBirth});
+            return student;
+        }
+    }
+}
