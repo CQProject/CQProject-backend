c94bfa2028b6e6f725e275b3e4871aec121a2359
ricardma <aluno18774@ipt.pt>
2017-08-04 18:32:36 +0100

    Changes in lessons, more authorizations

--- API/CQPROJ.Business/Queries/BLesson.cs
+++ API/CQPROJ.Business/Queries/BLesson.cs
@@ -34,55 +34,21 @@ namespace CQPROJ.Business.Queries
 
         }
 
-        public static Object GetAllLessonsByTeacher(int teacherID)
+        public static Object GetAllLessonsByStudent(int LessonID, int studentID)
         {
-            var schedules = db.TblSchedules.Select(x => x).Where(x => x.TeacherFK == teacherID);
-
-            var toSend = new List<Object>();
-            foreach (var schedule in schedules)
-            {
-                var les = db.TblLessons.Find(schedule.ID);
-
-                toSend.Add(new
-                {
-                    ID = les.ID,
-                    Day = les.Day,
-                    Homework = les.Homework,
-                    Observations = les.Observations,
-                    Summary = les.Summary
-                });
-            }
-            return toSend;
-
-        }
-
-        public static Object GetAllLessons(int pageID)
-        {
-            List<TblLessons> lessons;
             try
             {
-                lessons = db.TblLessons.Select(x => x).OrderBy(x => x.ID).Skip(50 * pageID).Take(50).ToList();
+                var lessons = db.TblLessonStudents.Find(LessonID, studentID);
+
+                return lessons;
             }
             catch (Exception)
             {
-                lessons = db.TblLessons.Select(x => x).OrderBy(x => x.ID).Skip(50 * pageID).ToList();
+                return null;
             }
-
-            var toSend = new List<Object>();
-            foreach (var lesson in lessons)
-            {
-                toSend.Add(new
-                {
-                    ID = lesson.ID,
-                    Day = lesson.Day,
-                    Homework = lesson.Homework,
-                    Observations = lesson.Observations,
-                    Summary = lesson.Summary
-                });
-            }
-            return toSend;
 
         }
+
 
         public static Object GetLessonProfile(int id)
         {
@@ -98,7 +64,6 @@ namespace CQPROJ.Business.Queries
 
         }
 
-        /**************************************NOT SURE**************************************************/
         public static void CreateLesson(Lesson lesson)
         {
             TblLessons les = new TblLessons
@@ -112,6 +77,32 @@ namespace CQPROJ.Business.Queries
 
             db.TblLessons.Add(les);
             db.SaveChanges();
+        }
+
+        public static Object EditLesson(int id,Lesson lesson)
+        {
+            try
+            {
+                var lessonFind = db.TblLessons.Find(id);
+
+                TblLessons les = new TblLessons
+                {
+                    Day = lesson.Day,
+                    Homework = lesson.Homework,
+                    Observations = lesson.Observations,
+                    Summary = lesson.Summary,
+                    ScheduleFK = lesson.ScheduleFK
+                };
+
+                db.TblLessons.Add(les);
+                db.SaveChanges();
+
+                return new { result = true };
+            }
+            catch (Exception)
+            {
+                return new { result = false, data = "Lição não encontrada" };
+            }
         }
     }
 }
