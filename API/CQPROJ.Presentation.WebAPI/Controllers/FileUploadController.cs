b9fec6b85239e8608ff4207cd32270c58766629b
davidbernardo <ravageiro@hotmail.com>
2017-08-24 15:36:39 +0100

    file upload

---
+++ API/CQPROJ.Presentation.WebAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace CQPROJ.Presentation.WebAPI.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        //POST fileupload/
+        [HttpPost]
+        [Route("fileupload")]
+        public Object UploadFile()
+        {
+            var fileName = "";
+            if (HttpContext.Current.Request.Files.AllKeys.Any())
+            {
+                // Get the uploaded image from the Files collection
+                var httpPostedFile = HttpContext.Current.Request.Files["file"];
+                
+                if (httpPostedFile != null)
+                {
+                    // Validate the uploaded image(optional)
+
+                    // Get the complete file path
+                    var fileFolder = HttpContext.Current.Server.MapPath("~/UploadedFiles").Replace("CQPROJ.Presentation.WebAPI", "CQPROJ.Business");
+                    fileName = Path.GetFileName(Guid.NewGuid() + httpPostedFile.FileName);
+                    var fileSavePath = Path.Combine(fileFolder, fileName);
+
+                    // Save the uploaded file to "UploadedFiles" folder
+                    httpPostedFile.SaveAs(fileSavePath);
+                    
+                }
+                
+            }
+
+            return new { result = true, data = fileName };
+        }
+
+    }
+}
