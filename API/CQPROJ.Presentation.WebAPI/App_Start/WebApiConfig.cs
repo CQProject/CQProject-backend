7ba0776b2d37931e4cdaa2287d1352d16ef82806
ricardma <aluno18774@ipt.pt>
2017-10-02 14:26:30 +0100

    Modifications on Startup.cs and WebApiConfig.cs

--- API/CQPROJ.Presentation.WebAPI/App_Start/WebApiConfig.cs
+++ API/CQPROJ.Presentation.WebAPI/App_Start/WebApiConfig.cs
@@ -12,7 +12,7 @@ namespace CQPROJ.Presentation.WebAPI
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
             config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
 
-            var corsAttr = new EnableCorsAttribute(origins: "http://localhost:4200", headers: "Content-Type, Authorization", methods: "GET, POST, PUT, DELETE");
+            var corsAttr = new EnableCorsAttribute(origins: "*", headers: "Content-Type, Authorization", methods: "GET, POST, PUT, DELETE");
             config.EnableCors(corsAttr);
 
             // Web API routes
